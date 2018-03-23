dc621425c7fa2f83d6e2225d91c7022228de8654
Maxim Balandin <balandus@bk.ru>
2018-03-23 14:39:59 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+    <title>Autopost8r</title>
+</head>
+<body>
+	<form action="./oauth/vkOauth.php" ><button>войти Через ВК</button></form><br>
+	<form action="./oauth/twitterOauth.php"><button>войти через Твиттер</button></form>
+</body>
---
+++ oauth/vkOauth.php
@@ -0,0 +1,54 @@
+    <?php
+
+    $client_id = '	6379963'; // ID приложения
+    $client_secret = 'jW97RHOk4MQcFh14f2mC'; // Защищённый ключ
+    $redirect_uri = 'http://localhost/laterpost'; // Адрес сайта
+
+    $url = 'http://oauth.vk.com/authorize';
+
+    $params = array(
+        'client_id'     => $client_id,
+        'redirect_uri'  => $redirect_uri,
+        'response_type' => 'code'
+    );
+
+    echo $link = '<p><a href="' . $url . '?' . urldecode(http_build_query($params)) . '">Аутентификация через ВКонтакте</a></p>';
+
+if (isset($_GET['code'])) {
+    $result = false;
+    $params = array(
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'code' => $_GET['code'],
+        'redirect_uri' => $redirect_uri
+    );
+
+    $token = json_decode(file_get_contents('https://oauth.vk.com/access_token' . '?' . urldecode(http_build_query($params))), true);
+
+   
+        if (isset($token['access_token'])) {
+		$params = array(
+		'ids' => $token['user_id'],
+		'fields' => 'id,first_name,last_name,screen_name,sex,bdate,photo_big',
+		'access_token' => $token['access_token'],
+		'v' => '5.73'
+			);
+
+
+        $userInfo = json_decode(file_get_contents('https://api.vk.com/method/users.get' . '?' . urldecode(http_build_query($params))), true);
+        if (isset($userInfo['response'][0]['id'])) {
+            $userInfo = $userInfo['response'][0];
+            $result = true;
+        }
+    }
+
+    if ($result) {
+        echo "Социальный ID пользователя: " . $userInfo['id'] . '<br />';
+        echo "Имя пользователя: " . $userInfo['first_name'] . '<br />';
+        echo "Ссылка на профиль пользователя: " . $userInfo['screen_name'] . '<br />';
+        echo "Пол пользователя: " . $userInfo['sex'] . '<br />';
+        echo "День Рождения: " . $userInfo['bdate'] . '<br />';
+        echo '<img src="' . $userInfo['photo_big'] . '" />'; echo "<br />";
+    }
+}
+?>
