028459254f82fb134761f725ff84e2c7fcc5c262
Maxim Balandin <balandus@bk.ru>
2018-03-24 16:54:17 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,22 +4,10 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
     <title>Autopost8r</title>
-	<script src="https://vk.com/js/api/openapi.js?152" type="text/javascript"></script>
 </head>
 <body>
-	<form action="./oauth/vkOauth.php" ><button>войти Через ВК</button></form><br>
-	<form action="./oauth/vk.php" ><button>войти Через dddВК</button></form><br>
+	<form action="./oauth/vk.php" ><button>войти Через ВК</button></form><br>
 	<form action="./oauth/twitterOauth.php"><button>войти через Твиттер</button></form>
-	<script type="text/javascript" src="//vk.com/js/api/openapi.js?152"></script>
-<script type="text/javascript">
-  VK.init({apiId: 6422262});
-</script>
-
-<!-- VK Widget -->
-<div id="vk_auth"></div>
-<script type="text/javascript">
-  VK.Widgets.Auth("vk_auth", {"onAuth":"function(data) {alert('user '+data['uid']+' authorized');}"});
-</script>
 	
  <form action="textarea1.php" method="post">
     <p><b>Текст поста:</b></p>
