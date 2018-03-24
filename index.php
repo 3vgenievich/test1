f2059177b6c886006dec5176cf527065d39ef24d
Maxim Balandin <balandus@bk.ru>
2018-03-24 15:45:32 +0500

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
 	<form action="./oauth/vkOauth.php" ><button>войти Через ВК</button></form><br>
 	<form action="./oauth/twitterOauth.php"><button>войти через Твиттер</button></form>
 	
-	
-    <script src="//ulogin.ru/js/ulogin.js"></script>
-<div id="uLogin" data-ulogin="display=panel;theme=classic;fields=first_name,last_name;providers=vkontakte,twitter;hidden=;redirect_uri=https%3A%2F%2Ftryinthatsht.herokuapp.com%2F;mobilebuttons=0;"></div>
+ <form action="textarea1.php" method="post">
+    <p><b>Введите ваш отзыв:</b></p>
+    <p><textarea rows="10" cols="45" name="text"></textarea></p>
+    <p><input type="submit" value="Отправить"></p>
+  </form>
 </body>
